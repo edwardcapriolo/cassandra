943f5ede6988090b3dbc8e40934da4eb3f26f8d3
Edward <edlinuxguru@gmail.com>
2017-04-12 12:11:00 -0400

    Cleanups

--- src/java/org/apache/cassandra/service/GCInspector.java
+++ src/java/org/apache/cassandra/service/GCInspector.java
@@ -335,24 +335,21 @@ public class GCInspector implements NotificationListener, GCInspectorMXBean
         }
     }
 
-    @Override
     public void setGcWarnThresholdInMs(long threshold)
     {
         if (threshold < 0)
-            throw new IllegalArgumentException("Threashold must be greater than 0");
+            throw new IllegalArgumentException("Threshold must be greater than or equal to 0");
         if (threshold != 0 && threshold <= gcLogThreshholdInMs)
-            throw new IllegalArgumentException("Threashold must be greater than gcLogTreasholdInMs which is currently " 
+            throw new IllegalArgumentException("Threshold must be greater than gcLogTreasholdInMs which is currently " 
                     + gcLogThreshholdInMs);
         gcWarnThreasholdInMs = threshold;
     }
 
-    @Override
     public long getGcWarnThresholdInMs()
     {
         return gcWarnThreasholdInMs;
     }
 
-    @Override
     public void setGcLogThresholdInMs(long threshold)
     {
         if (threshold <= 0)
@@ -363,13 +360,11 @@ public class GCInspector implements NotificationListener, GCInspectorMXBean
         gcLogThreshholdInMs = threshold;
     }
 
-    @Override
     public long getGcLogThresholdInMs()
     {
         return gcLogThreshholdInMs;
     }
 
-    @Override
     public long getStatusThresholdInMs()
     {
         return gcWarnThreasholdInMs != 0 ? gcWarnThreasholdInMs : gcLogThreshholdInMs;
